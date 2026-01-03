2a2808eee860a1a1c329c54a67f947db81363d77
h4nsln3r <68617162+h4nsln3r@users.noreply.github.com>
2026-01-04 00:47:38 +0100

    build fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,13 @@
 import { Parallax } from 'react-scroll-parallax';
 import heroImage from '../../assets/håj.jpg';
+import type { WeddingConfig } from '../../weddingConfig';
 import './hero.scss';
 
-export function Hero() {
+type HeroProps = {
+  wedding: WeddingConfig;
+};
+
+export function Hero({ wedding }: HeroProps) {
   return (
     <header className="hero">
       <Parallax speed={-50} className="hero__parallax" aria-hidden="true">
@@ -12,11 +17,11 @@ export function Hero() {
       <div className="hero__fade" aria-hidden="true" />
 
       <div className="hero__header">
-        <h1>Hannes</h1>
+        <h1>{wedding.couple.split(' & ')[0]}</h1>
         <h1>&</h1>
-        <h1>Julia</h1>
+        <h1>{wedding.couple.split(' & ')[1]}</h1>
         <br />
-        <p className="lead">2026 Augusti 29 Fredag</p>
+        <p className="lead">{wedding.dateLong}</p>
       </div>
     </header>
   );
