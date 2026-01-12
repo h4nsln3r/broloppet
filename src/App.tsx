083a2c94ce5aa67e2289427804f988cfab23dc3d
h4nsln3r <h4nsln3r@gmail.com>
2026-01-12 20:06:04 +0100

    Some chat gpt design updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { WEDDING } from "./weddingConfig";
 import { Information } from "./components/Section/Info";
 import { ToastMasters } from "./components/Section/Toast";
 import { RSVP } from "./components/Section/RSVP";
+import heartMark from "./assets/heart-mark.svg";
 
 export default function App() {
   return (
@@ -11,7 +12,15 @@ export default function App() {
       <Hero wedding={WEDDING} />
 
       <main className="content">
-        <h1 className="welcome">Välkommen till Hannes och Julias bröllop!</h1>
+        <div className="welcomeBlock">
+          <h1 className="welcome">Välkommen till Hannes och Julias bröllop!</h1>
+          <img className="welcomeBlock__heart" src={heartMark} alt="" />
+          <p className="welcomeBlock__intro muted">
+            Vi är så glada att du vill fira dagen med oss. Här hittar du tider,
+            plats, toastmasters och OSA – allt du behöver inför en fin dag
+            tillsammans.
+          </p>
+        </div>
         <Information />
 
         <ToastMasters />
--- src/components/Section/RSVP/index.tsx
+++ src/components/Section/RSVP/index.tsx
@@ -1,15 +1,34 @@
-import { WEDDING } from '../../../weddingConfig';
-import { RsvpForm } from '../../Form/RsvpForm';
+import { WEDDING } from "../../../weddingConfig";
+import { RsvpForm } from "../../Form/RsvpForm";
+import { ParallaxBanner } from "react-scroll-parallax";
+import rsvpBg from "../../../assets/hossmoka.jpg";
+
+import "../section.scss";
+import "./rsvp-section.scss";
 
 export function RSVP() {
   return (
-    <section className="section" id="rsvp">
+    <section className="section rsvpSection" id="rsvp">
       <h2>OSA / RSVP</h2>
       <p className="muted">
-        Svara om du kommer eller inte. Fyll gärna i allergier och önskemål kring mat.
+        Svara om du kommer eller inte. Fyll gärna i allergier och önskemål kring
+        mat.
       </p>
 
-      <RsvpForm osaDeadline={WEDDING.osaDeadline} />
+      <ParallaxBanner
+        className="rsvpBanner"
+        layers={[
+          {
+            image: rsvpBg,
+            speed: -15,
+          },
+        ]}
+      >
+        <div className="rsvpBanner__overlay" aria-hidden="true" />
+        <div className="rsvpBanner__content">
+          <RsvpForm osaDeadline={WEDDING.osaDeadline} />
+        </div>
+      </ParallaxBanner>
     </section>
   );
 }
